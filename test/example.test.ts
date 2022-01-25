0bd3debb3d780acfe271953909f272a9735acd1c
Harald Schwab <hs@rigbit.at>
2022-01-25 16:06:41 +0100

    fix error with eslint

--- test/example.test.ts
+++ test/example.test.ts
@@ -15,7 +15,7 @@ describe('example tests to check jest', () => {
 
   test('how cast to number with "+" in TypeScript works', () => {
     expect(+'abc' || 0).toBe(0); // NaN
-    const t: unknown = undefined;
+    const t: any = undefined;
     expect(+t || 1).toBe(1);
     expect(+'one' || 2).toBe(2); // NaN
     expect(+{val: 42} || 3).toBe(3); // NaN
